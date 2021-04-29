6551540e35dd96ff051e84a126bc39a37a6a54fa
liusilan2 <liusilan2@yy.com>
2021-04-29 09:36:00 +0800

    添加 virtual dom 逻辑

--- part3/slreact.js
+++ part3/slreact.js
@@ -7,44 +7,113 @@ function render(element, parentDom) {
   rootInstance = nextInstance;
 }
 
-function reconcile(parentDom, instance, element) {}
+function reconcile(parentDom, instance, element) {
+  if (instance == null) {
+    const newInstance = instantitate(element);
+    console.log("create dom ", parentDom, newInstance.dom);
+    parentDom.appendChild(newInstance.dom);
+    return newInstance;
+  } else if (element == null) {
+    console.log("remove dom");
+    // remove，若新子节点数 < 原节点数，需移除
+    parentDom.removeChild(instance.dom);
+    return null;
+  } else if (instance.element.type == element.type) {
+    console.log("reuse dom");
+    // 重用节点，更新属性
+    updateDomProperties(instance.dom, instance.element.props, element.props);
+    instance.childInstances = reconcileChildren(instance, element);
+    instance.element = element;
+    return instance;
+  } else {
+    console.log("replace dom");
 
-function instantitate(element) {}
+    // 替换原节点
+    const newInstance = instantitate(element);
+    parentDom.replaceChild(newInstance.dom, instance.dom);
+    return newInstance;
+  }
+}
 
-function render(element, parentDom) {
+// 对子节点做处理
+function reconcileChildren(instance, element) {
+  const dom = instance.dom;
+  const childInstances = instance.childInstances;
+  const nextChildElements = element.props.children || [];
+
+  const newChildInstances = [];
+
+  // 取最大的，若新子节点数 < 原节点数，需移除
+  const count = Math.max(childInstances.length, nextChildElements.length);
+  for (let i = 0; i < count; i++) {
+    const childInstance = childInstances[i];
+    const childElement = nextChildElements[i];
+
+    const newChildInstance = reconcile(dom, childInstance, childElement);
+    newChildInstances.push(newChildInstance);
+  }
+
+  return newChildInstances;
+}
+
+// virtual dom，保存真实的 dom，element，childInstance
+function instantitate(element) {
+  console.log("instantitate", element);
   const { type, props } = element;
-
-  // create dom element
   const isTextElement = type === TEXT_ELEMENT;
   const dom = isTextElement
     ? document.createTextNode("")
     : document.createElement(type);
 
-  // listener
-  const isListener = (name) => name.startsWith("on");
-  Object.keys(props)
-    .filter(isListener)
+  // 更新属性
+  updateDomProperties(dom, [], props);
+
+  // 处理子节点
+  const childElements = props.children || [];
+  const childInstances = childElements.map(instantitate);
+  const childDoms = childInstances.map((childInstance) => childInstance.dom);
+
+  childDoms.forEach((childDom) => dom.appendChild(childDom));
+
+  const instance = { dom, element, childInstances };
+
+  return instance;
+}
+
+// 更新 dom 属性
+function updateDomProperties(dom, prevProps, nextProps) {
+  const isEvent = (name) => name.startsWith("on");
+  const isAttribute = (name) => !isEvent(name) && name !== "children";
+
+  // remove listener
+  Object.keys(prevProps)
+    .filter(isEvent)
     .forEach((name) => {
-      const eventType = name.toLocaleLowerCase().substring(2);
-      dom.addEventListener(eventType, props[name]);
+      const eventType = name.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[name]);
     });
 
-  // property
-  const isAttribute = (name) => !isListener(name) && name != "children";
-  Object.keys(props)
+  // remove attributes
+  Object.keys(prevProps)
     .filter(isAttribute)
     .forEach((name) => {
-      dom[name] = props[name];
+      dom[name] = null;
     });
 
-  const childElements = props.children || [];
-  childElements.forEach((child) => render(child, dom));
+  // set attributes
+  Object.keys(nextProps)
+    .filter(isAttribute)
+    .forEach((name) => {
+      dom[name] = nextProps[name];
+    });
 
-  if (!parentDom.lastChild) {
-    parentDom.appendChild(dom);
-  } else {
-    parentDom.replaceChild(dom, parentDom.lastChild);
-  }
+  // add listener
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
 }
 
 // jsx 经 babel 转换后，会变成调用 createElement 的形式
@@ -74,8 +143,42 @@ function createTextElement(value) {
   return createElement(TEXT_ELEMENT, { nodeValue: value });
 }
 
+function render1(element, parentDom) {
+  console.log("render1");
+  const { type, props } = element;
+
+  // create dom element
+  const isTextElement = type === TEXT_ELEMENT;
+  const dom = isTextElement
+    ? document.createTextNode("")
+    : document.createElement(type);
+
+  // listener
+  const isListener = (name) => name.startsWith("on");
+  Object.keys(props)
+    .filter(isListener)
+    .forEach((name) => {
+      const eventType = name.toLocaleLowerCase().substring(2);
+      dom.addEventListener(eventType, props[name]);
+    });
+
+  // property
+  const isAttribute = (name) => !isListener(name) && name != "children";
+  Object.keys(props)
+    .filter(isAttribute)
+    .forEach((name) => {
+      dom[name] = props[name];
+    });
+
+  const childElements = props.children || [];
+  childElements.forEach((child) => render1(child, dom));
+
+  parentDom.appendChild(dom);
+}
+
 const SLReact = {
   render,
+  render1,
   createElement,
 };
 
